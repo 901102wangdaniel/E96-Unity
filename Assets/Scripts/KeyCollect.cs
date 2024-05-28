fd51f67bda0067d6d2820ab7da6bc323024f78fd
Musia <cshvager@protonmail.com>
2024-05-28 12:10:57 -0700

    key
    - beginnings of key collect

--- Assets/Scripts/KeyCollect.cs
+++ Assets/Scripts/KeyCollect.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MousePosValid : MonoBehaviour
+public class KeyCollect : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
