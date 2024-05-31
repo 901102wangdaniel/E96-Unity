c6cfb242e97742b29d527b93d6d4b65fc7faa6ae
Musia <cshvager@protonmail.com>
2024-05-31 10:41:58 -0700

    deleted key code

--- Assets/Scripts/KeyCollect.cs
+++ Assets/Scripts/KeyCollect.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class KeyCollect : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+// public class KeyCollect : MonoBehaviour
+// {
+//     // Start is called before the first frame update
+//     void Start()
+//     {
         
-    }
+//     }
 
-    // Update is called once per frame
-    void Update()
-    {
+//     // Update is called once per frame
+//     void Update()
+//     {
         
-    }
-}
+//     }
+
+
+
+// //put this in the player input
+// var itemCount : int;
+ 
+// function OnTriggerEnter(col : Colider) {
+//     if(col.tag=="TagName") {
+//       Destroy(col.gameObject);
+//       itemCount ++;
+//     }
+// }
+
+// }
